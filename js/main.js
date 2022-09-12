0630ff23e9164dfe5eddf41b1f08331af0183787
Felipe Calvo <fcalvob@udd.cl>
2022-09-12 01:17:00 -0300

    avance pre entrega 3

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* -------------------------------------------------------------------------- */
+/*                             NAVEGADOR ANIMACION                            */
+/* -------------------------------------------------------------------------- */
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+    
+})
